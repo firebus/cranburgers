c78b7f122f043e4b719d23a7a2b88d0d6f247ee8
firebus <russ@firebus.com>
2011-08-27 14:20:42 -0700

    finally adding a non-working like button to the news feed

---
+++ Readme.md
@@ -0,0 +1,31 @@
+Metalike
+========
+
+#### Metalike, by Cranburgers
+
+- This project will allow you to like the fact that someone likes something on Facebook.
+- Additionally you can like the fact that someone likes that someone likes something
+- Ad Infinitum
+
+### Howto
+
+- Visit http://cranburgers.no.de and bookmark the bookmarklet
+- Visit http://facebook.com and click the bookmarklet
+
+### Todo
+
+- Display metalike button where appropriate
+  - Working on news feed, but not on other pages yet
+- Save metalike to datastore when metalike button is clicked
+  - Not yet
+- Display metalikes 
+  - Not yet
+- Support meta-metalikes
+  - Not yet
+- Greasemonky and other plugins
+  - Not yet
+- Un-metalike
+  - Not yet
+- Real-time updates
+  - Not yet
+
--- metalike.js
+++ metalike.js
@@ -7,22 +7,75 @@
  */
 
 /**
+ * Why am I doing this and not using jQuery?
+ */
+var $$ = function () {
+  return document.querySelectorAll.apply(document, arguments)
+};
+ 
+/**
  * Once libs are loaded, open a socket and do something trivial
  */
-function openSocket() {
+function initializeMetalike() {
+  attachMetalikeButtons();
+  
   if (typeof io == 'undefined') {
 	  // set to check every 100 milliseconds if the libary has loaded
-	  window.setTimeout(openSocket, 100);
+	  window.setTimeout(initializeMetalike, 100);
 	}
 	else {
 	  var socket = io.connect('http://localhost:7777');
     socket.on('news', function (data) {
       //console.log(data);
-      alert("Hello, " + data['hello']);
+      //alert("Hello, " + data['hello']);
     });
   }
-}	
- 
+}
+
+/**
+ * A test function that edits the innerHTML of the existing like button
+ */
+function insultLikers() {
+	var likes = $$('.like_link span.default_message, .cmnt_like_link span.default_message');
+	if (typeof likes != 'undefined') {
+    for (var like in likes) { 
+      if (likes[like].firstChild) {
+        likes[like].firstChild.nodeValue = "Like it, you big fat ape!"; 
+      }
+    }
+  }
+}
+
+/**
+ * Create a metalike button
+ */
+function createMetalikeButton() {
+  var metaButton = document.createElement('BUTTON');
+  metaButton.innerHTML = '<span>Like</span>';
+  metaButton.setAttribute('class', 'as_link');
+  return metaButton;
+}
+
+function createSeparator() {
+  return document.createTextNode(' · ');
+}
+
+/**
+ * Attach metalike button to all likes
+ */
+function attachMetalikeButtons() {
+  var likes = $$('li.uiUfiLike div.UIImageBlock_Content');
+  var count = 0;
+  if (typeof likes != 'undefined') {
+    for (var like in likes) {
+      count++;
+      likes[like].appendChild(createSeparator());
+      likes[like].appendChild(createMetalikeButton());
+    }
+    //alert(count + ' likes');
+  }
+}
+
 /**
  * Dynamically create script tags to load external libraries
  * - socket.io.js
@@ -33,6 +86,6 @@ function loadSocketIo() {
   proto.src = 'http://' + hostname + '/socket.io/socket.io.js';
   var dhead = document.getElementsByTagName('head')[0] || document.documentElement;
   dhead.insertBefore(proto, dhead.firstChild);
-  openSocket();
+  initializeMetalike();
 }
 window.addEventListener("load", loadSocketIo());
